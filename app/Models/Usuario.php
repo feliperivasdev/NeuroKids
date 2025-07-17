4caf77fa21be60202e4cd26eac80eb88b1c8e8d8
Felipe Rivas <79724739+feliperivasdev@users.noreply.github.com>
2025-07-16 20:49:12 -0500

    Add JWT and authentication support to Usuario model
    Implements Authenticatable and JWTSubject interfaces in the Usuario model to enable authentication and JWT functionality. Adds methods for JWT identifier, custom claims, and overrides password attribute for authentication.

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Usuario extends Model
+class Usuario extends Model implements AuthenticatableContract, JWTSubject
 {
-    use HasFactory;
+    use HasFactory, Authenticatable;
 
     /**
      * La tabla asociada al modelo.
@@ -57,5 +60,33 @@ class Usuario extends Model
     public function getKeyName()
     {
         return 'id';
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    /**
+     * Override the password attribute name for authentication
+     */
+    public function getAuthPassword()
+    {
+        return $this->contrasena_hash;
     }
 }
